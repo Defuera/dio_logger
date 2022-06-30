65c65f1e30bffda0c3a6108936fb55c1cf6bf8eb
Abdelrahman Mostafa Elmarakby <43115402+abdelrahmanelmarakby@users.noreply.github.com>
2022-06-30 08:35:58 +0200

    Update dio_logger.dart

--- lib/dio_logger.dart
+++ lib/dio_logger.dart
@@ -1,26 +1,43 @@
-library dio_logger;
-
 import 'package:dio/dio.dart';
-
-final dioLoggerInterceptor = InterceptorsWrapper(onRequest: (RequestOptions options, handler) {
+import 'package:logger/logger.dart';
+///intial settings for the logger 
+Logger logger = Logger(
+  printer: PrettyPrinter(
+    methodCount: 2,
+    errorMethodCount: 8,
+    lineLength: 40,
+    colors: true,
+    printEmojis: true,
+  ),
+);
+///DIO logger interceptor to log all request details with sections,coloring and emojis.
+final dioLoggerInterceptor =
+    InterceptorsWrapper(onRequest: (RequestOptions options, handler) {
   String headers = "";
   options.headers.forEach((key, value) {
     headers += "| $key: $value";
   });
 
-  print("┌------------------------------------------------------------------------------");
-  print('''| [DIO] Request: ${options.method} ${options.uri}
+  logger.v(
+      "┌------------------------------------------------------------------------------");
+  logger.v('''| [DIO] Request: ${options.method} ${options.uri}
 | ${options.data.toString()}
 | Headers:\n$headers''');
-  print("├------------------------------------------------------------------------------");
-  handler.next(options);  //continue
+  logger.v(
+      "├------------------------------------------------------------------------------");
+  handler.next(options); //continue
 }, onResponse: (Response response, handler) async {
-  print("| [DIO] Response [code ${response.statusCode}]: ${response.data.toString()}");
-  print("└------------------------------------------------------------------------------");
+  logger.i(
+      "| [DIO] Response [code ${response.statusCode}]: ${response.data.toString()}");
+  logger.i(
+      "└------------------------------------------------------------------------------");
   handler.next(response);
   // return response; // continue
 }, onError: (DioError error, handler) async {
-  print("| [DIO] Error: ${error.error}: ${error.response?.toString()}");
-  print("└------------------------------------------------------------------------------");
+  logger.wtf("---------------------------------------");
+  logger.wtf(
+      "| [DIO] Error: ${error.error}: ${error.response?.toString()}", error);
+  logger.wtf(
+      "└------------------------------------------------------------------------------");
   handler.next(error); //continue
 });
